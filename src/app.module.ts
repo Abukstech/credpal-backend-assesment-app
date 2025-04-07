24e8c74d917f2494d7a208e3e8a77d76ac346804
Abukstech <abdulmajeedbamidele4@gmail.com>
2025-04-07 16:31:03 -0700

    set up type orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.prod'],
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,36 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+const isProductionEnvironment = process.env.NODE_ENV === 'production';
+
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+
+  host: isProductionEnvironment ? process.env.MYSQL_HOST : process.env.MYSQL_HOST_LOCAL,
+  username: isProductionEnvironment ? process.env.MYSQL_USER :  process.env.MYSQL_USER_LOCAL,
+  database: isProductionEnvironment ? process.env.MYSQL_DATABASE :  process.env.MYSQL_DATABASE_LOCAL,
+  password: isProductionEnvironment ? process.env.MYSQL_PASSWORD :  process.env.MYSQL_PASSWORD_LOCAL,
+  
+  // host: process.env.PG_HOST,
+  // username: process.env.PG_USER,
+  // database: process.env.PG_DATABASE,
+  // password: process.env.PG_PASSWORD,
+  
+  // url: process.env.MYSQL_URL,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  migrations: [__dirname + '/../database/migrations/*.entity.{.ts,.js}'], // Migration files directory
+  // migrationsRun: true,
+  // migrationsTableName: 'migrations_TypeORM',
+  extra: {
+    charset: 'utf8mb4_unicode_ci',
+  },
+  synchronize: false, // Auto-create database tables based on entities (not recommended for production)
+  // autoLoadEntities: true, // Automatically load entity files
+  logging: false, // Disable logging SQL queries
+  // ssl: {
+  //   rejectUnauthorized: false, // Reject unauthorized SSL connections
+  // },
+};
