5f65ed617d62ba9f93d185c0d545d863cf0637c9
Abukstech <abdulmajeedbamidele4@gmail.com>
2025-04-10 03:51:21 -0700

    fix the response cookie

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -57,7 +57,7 @@ export class AuthService {
         response.cookie('access_token', token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
-          sameSite: 'strict',
+          sameSite: 'none',
           maxAge: 24 * 60 * 60 * 1000 // 1 day
         });
     
